ddf249a5504422f229e1d5066cd08ad5ab65db33
Shivam Ojha <shivamojha1422000@gmail.com>
2020-11-09 15:20:47 +0530

    login system added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(methodOverride("_method"));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+var User = require("./models/user.js");
 
 
 
@@ -31,6 +32,40 @@ var BlogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Data",BlogSchema);
 
+
+//importing routes:-
+
+var authRoutes = require("./routes/authentication");
+app.use(authRoutes);
+// ******************************88
+
+
+
+//Passport Configuration:-
+// ====================================
+var passport= require("passport");
+var LocalStrategy= require("passport-local");
+
+app.use(require("express-session")({
+		secret:"HackABit",
+	    resave:false,
+	    saveUninitialized:false
+		}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// app.use(function(req,res,next){
+// 	res.locals.currentUser = req.user;
+// 	res.locals.error=req.flash("error");
+// 	next();
+// })
+// *****************************************
 
 
 
@@ -159,10 +194,10 @@ app.delete("/blogs/:id",function(req,res){
 
 
 // ************************************* Post Route Ended****************************
-// app.listen("3000",function(){
-// 	console.log("Server Started");
-// });
+app.listen("3000",function(){
+	console.log("Server Started");
+});
 
 // app.listen(process.env.port,process.env.ip);
 
-app.listen(process.env.PORT,process.env.IP);
+// app.listen(process.env.PORT,process.env.IP);
---
+++ routes/authentication.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var router = express.Router();
+// var Cg= require("../models/campgrounds");
+var passport=require("passport");
+var User = require("../models/user");
+// router.set("view engine","ejs");
+
+// AuthenticatiRoutesute
+// ++++++++++++++++++++++++++++++++++++++++++
+
+router.get("/register",function(req,res){
+	res.render("register");
+	
+})
+
+router.post("/register",function(req,res){
+	var newUser=new User({username:req.body.username});
+User.register(newUser,req.body.password,function(err,user){
+	if(err){
+		console.log(err);
+		return res.render("register");
+	}else{
+		passport.authenticate("local")(req,res,function(){
+			res.redirect("/");
+		})
+	}
+})
+})
+// ***************************************
+
+// logAuthenticatiRoutesuteute
+// ====================================
+router.get("/login",function(req,res){
+	
+	res.render("login");
+})
+
+
+router.post("/login",passport.authenticate("local",{
+	successRedirect: "/",
+	failureRedirect:"/login"
+}),function(req,res){
+	
+});
+// *************************************************
+
+// logout
+// =====================
+router.get("/logout",function(req,res){
+	req.logout();
+	res.redirect("/");
+	
+});
+
+// is logged function
+function isLoggedIn(req,res,next){
+	if(req.isAuthenticated()){
+		return next();
+	}else{
+		res.redirect("/login");
+	}
+	
+}
+// **********************************
+
+	module.exports = router;
